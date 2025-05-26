0a8e6ef07cfae80318a7ce7c9cce69b49dc7f156
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-27 01:15:52 +0800

    remix2025with0705mui0418 init

---
+++ remixwithbulma20250705-git.bat
@@ -0,0 +1,13 @@
+git init
+
+git add .
+ 
+git commit -m "remix2025with0705mui0418 init"
+
+git remote add origin https://github.com/goldshoot0720/remix2025with0705mui0418.git
+
+git checkout -b master
+
+git push -u origin master --force
+
+pause
---
+++ remixwithbulma20250705-open.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\chbon\Documents\20250705\remix2025with0705mui0418"
+npm run dev -- --open
+pause
